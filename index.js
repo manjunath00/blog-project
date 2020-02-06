81ddd6b68c23b285138de9cc9449375b27d26192
manjunath00 <kadrimanjunath56@gmail.com>
2020-02-06 16:00:02 +0530

    installed express & mongoose

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3200;
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
+
+app.listen(PORT, () => console.log(`Up & running at http://localhost:${PORT}`))
