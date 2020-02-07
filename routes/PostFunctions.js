3e550edb1f9a9097917faf541bf3dc043c286d87
manjunath00 <kadrimanjunath56@gmail.com>
2020-02-07 18:36:54 +0530

    completed all requests & response function for post collection

--- routes/PostFunctions.js
+++ routes/PostFunctions.js
@@ -1,9 +1,11 @@
 const Post = require("../models/PostModel");
+const User = require("../models/UserModel");
 
+// get all posts
 const getAllPosts = async (req, res) => {
   try {
     const allPosts = await Post.find();
-
+    console.log(allPosts);
     res.status(200).json({
       status: "success",
       results: allPosts.length,
@@ -14,11 +16,13 @@ const getAllPosts = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: "fail",
-      message: err
+      message: err.message,
+      err
     });
   }
 };
 
+// get a post by id
 const getAPost = async (req, res) => {
   try {
     const postId = req.params.postId;
@@ -33,16 +37,22 @@ const getAPost = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: "fail",
-      message: err
+      message: err.message,
+      err
     });
   }
 };
 
+// update a post
 const updateAPost = async (req, res) => {
   try {
-    const { postId, body } = req.body;
-    const results = await Post.findByIdAndUpdate(postId, body);
+    const postId = { _id: req.params.id };
+    const body = req.body;
+    const results = await Post.findByIdAndUpdate(req.params.id, {$set: body }, {
+      returnNewDocument: true
+    });
 
+    console.log(results);
     res.status(200).json({
       status: "success",
       data: {
@@ -52,14 +62,15 @@ const updateAPost = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: "fail",
-      message: err
+      message: err.message,
+      err
     });
   }
 };
 
 const deleteAPost = async (req, res) => {
   try {
-    const postId = request.params.postId;
+    const postId = req.params.postId;
     const results = await Post.findByIdAndDelete(postId);
 
     res.status(200).json({
@@ -71,25 +82,35 @@ const deleteAPost = async (req, res) => {
   } catch (err) {
     res.status(404).json({
       status: "fail",
-      message: err
+      message: err.message,
+      err
     });
   }
 };
 
+// create a post & check if the author exists in users collection
 const newPost = async (req, res) => {
   try {
     const { body } = req;
-    const results = await Post.create(body);
-    res.status(200).json({
-      status: "success",
-      data: {
-        results
-      }
-    });
-  } catch (err) {
+    const { author } = body;
+    const isUserExists = await User.findOne({ username: author });
+
+    if (isUserExists) {
+      const results = await Post.create(body);
+      res.status(200).json({
+        status: "success",
+        data: {
+          results
+        }
+      });
+    } else {
+      throw new Error("User doesn't exist. You need to create account");
+    }
+  } catch (error) {
     res.status(404).json({
       status: "fail",
-      message: err
+      message: error.message,
+      error
     });
   }
 };
