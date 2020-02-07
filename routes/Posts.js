82df81f89490503cb3eefd5a148a103382c57ba0
manjunath00 <kadrimanjunath56@gmail.com>
2020-02-07 14:43:09 +0530

    Added req & res for Post

---
+++ routes/Posts.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllPosts,
+  getAPost,
+  updateAPost,
+  deleteAPost,
+  newPost
+} = require("./PostFunctions");
+
+router.get("/", getAllPosts);
+router.post("/", newPost);
+router.get("/:postId", getAPost);
+router.put("/:postId", updateAPost);
+router.delete("/:postId", deleteAPost);
+
+module.exports = router;
+
