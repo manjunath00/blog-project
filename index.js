3636a59533c8676985b444a9993bab8bcc38256d
manjunath00 <kadrimanjunath56@gmail.com>
2020-02-07 10:14:24 +0530

    Added connection to the database

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const DB = "mongodb://localhost:27017/postDatabase";
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+  })
+  .then(results => {
+    console.log("connection was successfull");
+    console.log("connection was successfull");
+  })
+  .catch(err => {
+    console.log(err);
+    console.log("connection failed");
+  });
 
 const PORT = process.env.PORT || 3200;
 
