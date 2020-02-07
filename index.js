3effd5fe4c742902f26ed457645abababc144459
manjunath00 <kadrimanjunath56@gmail.com>
2020-02-07 17:15:23 +0530

    Moved database config to separate file

--- index.js
+++ index.js
@@ -1,56 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
 // custom imports
+const { DB, options } = require("./config/database");
 const app = express();
-const DB = "mongodb://localhost:27017/postDatabase";
 
-const Post = require("./models/Post");
-const User = require("./models/User");
-
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true
-  })
-  .then(results => {
-    // console.log(results);
-    console.log("connected to database");
-  })
-  .catch(err => {
-    // console.log(err);
-    console.log("connection failed");
-  });
-
-const user = new User({
-  _id: new mongoose.Types.ObjectId(),
-  username: "John cena"
+mongoose.connect(DB, options).then(results => {
+  // console.log(results);
+  console.log("connected to database");
 });
 
-user.save(function(err) {
-  if (err) {
-    return err;
-  }
+// express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-  const post = new Post({
-    title: "lorem ipsum",
-    body:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vehicula tellus eget diam porttitor lobortis. In sodales libero a est luctus consectetur. Duis lorem est, tempor at ligula sed, scelerisque faucibus eros. Integer convallis, felis sed pretium faucibus, dui felis malesuada quam, a dignissim.",
-    author: user._id
-  });
-  
-  post.save(function(err) {
-    console.log('executing post function')
-    if (err) {
-      return err;
-    }
-  });
-});
+// posts routes
+app.use("/api/posts", require("./routes/Posts"));
+
+// users routes
+app.use("/api/users", require("./routes/Users"));
 
 const PORT = process.env.PORT || 3200;
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
+  res.send("Blog");
 });
 
 app.listen(PORT, () => console.log(`Up & running at http://localhost:${PORT}`));
