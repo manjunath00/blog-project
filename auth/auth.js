c6d7bc71b79658842a9e212e2228917390cb3464
manjunath00 <kadrimanjunath56@gmail.com>
2020-02-10 21:17:46 +0530

    Added validateToken middleware

--- auth/auth.js
+++ auth/auth.js
@@ -1,7 +1,8 @@
 const jwt = require("jsonwebtoken");
 const secretKey = process.env.SECRET_KEY || "RmOeBolEiltZELionJuMEntErdanImEg";
+const { redisClient } = require("../config/database.js");
 
-const validateToken = (req, res, next) => {
+const validateToken = async (req, res, next) => {
   const authorizationHeader = req.headers.authorization;
   let result;
 
@@ -10,15 +11,25 @@ const validateToken = (req, res, next) => {
 
     try {
       result = jwt.verify(token, secretKey);
-
       // send it to req
       console.log(result);
       req.decoded = result;
       req.username = result.userData.username;
-      console.log(req);
-      next();
-    } catch (error) {
-      throw new Error(error);
+      console.log(req.username)
+      const isLoggedOut = await redisClient.hget("LoginTokens", req.username);
+      console.log(isLoggedOut)
+
+      if(isLoggedOut) {
+        req.email = result.userData.email; 
+        next();
+      }else {
+        throw new Error({ msg : "User already logged out "})
+      }
+    } catch (error) { 
+      res.json({
+        status: "fail",
+        error
+      })
     }
   } else {
     // no token
@@ -30,6 +41,8 @@ const validateToken = (req, res, next) => {
     res.status(401).send(result);
   }
 };
+
+// delete the token
 
 module.exports = {
   validateToken
