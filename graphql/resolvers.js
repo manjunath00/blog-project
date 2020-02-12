f0d198731b9e0f0d908c144500786a297db16e6a
manjunath00 <kadrimanjunath56@gmail.com>
2020-02-12 20:31:52 +0530

    Added logIn and register user functionality

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,6 +1,53 @@
 const User = require("../models/UserModel");
 const Post = require("../models/PostModel");
+const jwt = require("jsonwebtoken");
+const secretKey = process.env.SECRET_KEY;
+const expiresIn = process.env.EXPIRES_IN;
 
 module.exports = {
+  createUser: async function({ userInput }, req) {
+    try {
+      const isExistingUser = await User.findOne({ email: userInput.email });
+      if (isExistingUser) {
+        throw new Error("User already exists");
+      }
 
+      const user = new User({
+        username: userInput.username,
+        email: userInput.email,
+        password: userInput.password
+      });
+
+      const createdUser = await user.save();
+      return {
+        ...createdUser._doc,
+        _id: createdUser._id.toString()
+      };
+    } catch (error) {
+      return error;
+    }
+  },
+
+  logIn: async function({ userInput }, req) {
+    try {
+      const user = await User.findOne({ email: userInput.email });
+      console.log(user);
+
+      const { email, username } = user;
+
+      if (user.password === userInput.password) {
+        const token = jwt.sign({ email, username }, secretKey, { expiresIn });
+
+        console.log(token);
+        return {
+          _id: user._id.toString(),
+          token,
+          username,
+          email
+        };
+      }
+    } catch (error) {
+      return error;
+    }
+  }
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,5 +1,49 @@
 const { buildSchema } = require("graphql");
 
-module.exports = buildSchema(`
-    
-  `);
+module.exports = buildSchema(`  
+  type Post {
+    _id : ID
+    title : String!
+    body : String!
+    username : User!
+  }
+
+  type User {
+    _id : ID!
+    username : String!
+    email : String!
+    password : String
+  }
+
+  type AuthData {
+    _id : ID!
+    token: String! 
+    username: String!
+    email: String!
+  }
+
+  input LogInData {
+    username : String!
+    email : String!
+    password : String!
+  }
+   
+  input userInputData {
+    email : String!
+    username : String!
+    password : String!
+  }
+
+  type RootMutation {
+    createUser(userInput: userInputData) : User!
+  }
+
+  type RootQuery {
+    logIn(userInput: LogInData) : AuthData!
+  }
+
+  schema {
+    mutation : RootMutation
+    query : RootQuery
+  }
+`);
